1e87105c8b877aa4d863901d499d4cd03f5505db
Clint Brodar <clintbrodar@gmail.com>
2019-10-04 08:53:26 -0700

    Initial files

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mysql = require("mysql");
+const path = require("path");
+require('dotenv').config();
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+var connection;
+var env = process.env.JAWSDB_URL;
+connection = mysql.createConnection(env);
+connection.connect(function(err) {
+  if (err) {
+    console.error("Connection error: " + err.stack);
+    return;
+  }
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
