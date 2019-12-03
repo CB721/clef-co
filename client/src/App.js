ad867b1c0baca7812135ffdbadc0531be972c80e
Clint Brodar <clintbrodar@gmail.com>
2019-12-03 13:00:04 -0800

    Cookie notification

--- client/src/App.js
+++ client/src/App.js
@@ -14,37 +14,55 @@ import ProductPage from "./pages/ProductPage";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import User from "./pages/User";
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { css } from 'glamor';
 import './App.css';
 
 
 function App() {
   const products = useSelector(state => state.products);
+  if (!window.sessionStorage.cookie_notification) {
+    toast("This website stores data such as cookies to enable important site functionality including analytics, targeting, and personalization.  Click to confirm.", {
+      closeOnClick: true,
+      autoClose: false,
+      className: css({
+        background: '#3E0768',
+        boxShadow: '2px 2px 20px 2px rgba(0,0,0,0.3)',
+        borderRadius: '17px'
+      }),
+      bodyClassName: css({
+        fontSize: '20px',
+        color: 'white'
+      })
+    });
+    sessionStorage.setItem("cookie_notification", true);
+  }
 
   return (
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/products" component={Products} />
-          <Route exact path="/tutorials" component={Tutorials} />
-          <Route exact path="/shop" component={Shop} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/cart" component={Cart} />
-          {products.map(product => (
-            <Route
-              key={product.id}
-              exact path={`/shop/product/${product.id}`}
-              component={ProductPage}
-            />
-          ))}
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/create_account" component={SignUp} />
-          <Route exact path="/user/profile" component={User} />
-        </Switch>
-        <Footer />
-      </Router>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/tutorials" component={Tutorials} />
+        <Route exact path="/shop" component={Shop} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/cart" component={Cart} />
+        {products.map(product => (
+          <Route
+            key={product.id}
+            exact path={`/shop/product/${product.id}`}
+            component={ProductPage}
+          />
+        ))}
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/create_account" component={SignUp} />
+        <Route exact path="/user/profile" component={User} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
--- schemas/order_seeds.sql
+++ schemas/order_seeds.sql
@@ -6,9 +6,129 @@ INSERT INTO oxn711nfcpjgwcr2.orders
 )
 VALUES
 (
-    3,
-    "2019-11-09T09:32:12",
-    "2019-11-09T09:41:45"
+    1,
+    "2013-09-23T09:32:12",
+    "2013-09-23T09:41:45"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    9,
+    "2019-12-02T05:58:12",
+    "2019-12-02T06:02:12"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    17,
+    "2019-11-29T11:12:12",
+    "2019-11-29T11:31:05"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    15,
+    "2019-10-19T15:24:02",
+    "2019-10-19T15:31:25"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    16,
+    "2019-09-19T14:50:02",
+    "2019-09-19T14:52:31"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    8,
+    "2019-12-01T17:55:52",
+    "2019-12-01T18:01:21"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    12,
+    "2019-12-01T04:12:32",
+    "2019-12-01T04:31:46"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    11,
+    "2019-12-02T09:32:12",
+    "2019-12-02T09:41:45"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    10,
+    "2019-06-21T06:32:12",
+    "2019-06-21T07:41:45"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    17,
+    "2019-01-25T12:40:02",
+    "2019-01-25T12:45:55"
+);
+INSERT INTO oxn711nfcpjgwcr2.orders
+(
+    user_id,
+    created_at,
+    checked_out_at
+)
+VALUES
+(
+    14,
+    "2019-04-19T12:22:42",
+    "2019-04-19T12:31:15"
 );
 
 INSERT INTO oxn711nfcpjgwcr2.orderItems
@@ -18,8 +138,5 @@ INSERT INTO oxn711nfcpjgwcr2.orderItems
     order_id
 )
 VALUES
-(
-    1,
-    4,
-    1
-);
+(1, 1, 1),
+;
